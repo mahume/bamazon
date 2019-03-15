6de2257ff81d2b2b6ed9eb81af37b77756c25c79
mahume <michael.a.hume@gmail.com>
2019-03-15 16:20:58 -0600

    Qty check

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const mysql = require('mysql')
 const inquirer = require('inquirer')
+const chalk = require('chalk')
+const ora = require('ora')
 
 const options = {
     host: 'localhost',
@@ -25,28 +27,41 @@ function selectAll() {
             throw err
         }
         console.table(res)
-        connection.end()
+        questions()
+    })
+}
+function questions() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: `What is the ID# of the product that you'd like to buy?`
+        },
+        {
+            type: 'input',
+            name: 'item_qty',
+            message: `How many would you like to buy?`
+        }
+    ])
+    .then(answers => {
+        productCheck(answers.item_id, answers.item_qty)
     })
 }
 
-inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'item_id',
-        message: `What is the ID# of the product that you'd like to buy?`
-    },
-    {
-        type: 'input',
-        name: 'item_qty',
-        message: `How many would you like to buy?`
-    }
-])
-.then(answers => {
-    productCheck(answers.item_id, answers.item_qty)
-})
 
 function productCheck(id, qty) {
-    console.log(id)
-    console.log(qty)
+    const query = connection.query(`SELECT * FROM products WHERE item_id=?`, [id], (err, res) => {
+        const item_id = parseInt(id, 10)
+        const quantity = parseInt(qty, 10)
+
+        if (res[0].stock_quantity >= quantity) {
+            console.log('Sufficient stock')
+        } else {
+            console.log('Insufficient Qty')
+        }
+    })
 }
+
+
+// connection.end()
